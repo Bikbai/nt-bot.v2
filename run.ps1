809d589f6826d67a092b1dbbc0151be2b2dbbb6d
Demidov <demidov.roman@gmail.com>
2022-10-28 11:15:58 +0300

    Стартап, отладка

--- run.ps1
+++ run.ps1
@@ -1,11 +1,16 @@
+Write-Host "Апгрейд питонов"
 python.exe -m pip install --upgrade pip
+Write-Host "Настройка виртуальной инфраструктуры venv"
 python -m venv venv
+Write-Host "Активация виртуализации"
 venv\Scripts\activate
+Write-Host "Апгрейд необходимых для бота пакетов"
 pip install --upgrade -r requirements.txt
-
-if (Test-Path 'env:BOT_TOKEN' == False) {
-    $path = Read-Host -Prompt 'Введите секретный токен бота:'
+Write-Host "Проверка наличия переменной окружения"
+if (-not (Test-Path 'env:BOT_TOKEN')) {
+    $path = Read-Host -Prompt 'Введите секретный токен бота'
     $env:BOT_TOKEN= $path
 }
+Write-Host "Стартуем"
 python .\src\main.py
 venv\Scripts\deactivate
